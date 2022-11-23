0abbca2b6fd615f07964e6b72bfc536707cb98ba
liupoyip <liupoyip@gmail.com>
2022-11-23 20:39:56 +0800

    LGTM!!

--- model/NIDAQ.py
+++ model/NIDAQ.py
@@ -20,30 +20,7 @@ from nidaqmx.constants import (AcquisitionType,
 import numpy as np
 import numpy.typing as npt
 
-
-class NiCallbackDataList(list):
-    """
-    Properties:
-        list[0]: NiStreamReaders.AnalogMultiChannelReader
-            stream_reader for nidaqmx
-            ```
-            stream_reader = NiStreamReaders.AnalogMultiChannelReader(nidaqmx.Task.in_stream)
-            ```
-        list[1]: numpy.array
-            chunk for acquiring data
-
-    Limitation: max length is 2
-    """
-
-    def __init__(self):
-        self.max_len = 2
-
-    def append(self, item):
-        if len(self) < self.max_len:
-            super().append(item)
-        else:
-            raise BaseException(
-                f'Too many elements. {self.__class__.__name__} max length is {self.max_len} ')
+from .utils import CSVStreamWriter
 
 
 @dataclasses.dataclass
@@ -60,12 +37,12 @@ class NIDAQ(GeneralDAQParams):
     stream_trig: Optional[bool] = None
     task: Optional[nidaqmx.task.Task] = None
     device: Optional[nidaqmx.system.device.Device] = None
-    callback_data: Optional[NiCallbackDataList] = None
-    callback_data_ptr: Optional[ctypes.py_object] = None
     device_name: Optional[str] = None
     min_sample_rate: Optional[float] = None
     max_sample_rate: Optional[float] = None
     stream_reader: Optional[NiStreamReaders.AnalogMultiChannelReader] = None
+    stream_writer: Optional[CSVStreamWriter] = None
+    write_file_trig: Optional[bool] = None
 
     def __init__(self) -> None:
         super(NIDAQ, self).__init__()
@@ -114,20 +91,15 @@ class NIDAQ(GeneralDAQParams):
     def show_daq_chassis_device_name(self) -> None:
         print(f'DAQ chassis name: {self.device.compact_daq_chassis_device}')
 
-    def create_task(self, task_name: str) -> None:
-        self.task = nidaqmx.task.Task(new_task_name=task_name)
-
-    def clear_task(self) -> None:
-        if self.task != None:
-            self.task.close()
-
 
 class NI9234(NIDAQ):
     channel_num_list: tuple = (0, 1, 2, 3, '0', '1', '2', '3')
-    chunk: npt.NDArray[np.float32]
+    chunk: npt.NDArray[np.float64]
+    ptr: Optional[ctypes.py_object]
 
     def __init__(self, device_name) -> None:
         super(NI9234, self).__init__()
+
         self.device_name = device_name
         self.device = nidaqmx.system.device.Device(device_name)
         self.device.reset_device()
@@ -135,13 +107,17 @@ class NI9234(NIDAQ):
         self.sample_rate = 12800
         self.record_duration = 5.0
         self.frame_duration = 1000  # millisecond
-        # max/min sampling rate of multi-channel and single-channel is same in NI-9234
         self.min_sample_rate = self.device.ai_min_rate
         self.max_sample_rate = self.device.ai_max_single_chan_rate
         self.frame_size = int(self.sample_rate * self.frame_duration * 0.001)
         self.buffer_size = self.frame_size * 10
-        self.callback_data = NiCallbackDataList()
-        self.callback_data_ptr = ctypes.py_object(self.callback_data)
+
+        self.write_file_trig = False
+        self.write_file_dir = '.'
+
+        self.stream_writer = CSVStreamWriter(directory=self.write_file_dir)
+
+        self.self_ptr = ctypes.py_object(self)
         self.stream_trig = False
 
     def create_task(self, task_name: str) -> None:
@@ -209,14 +185,19 @@ class NI9234(NIDAQ):
         self.buffer_size = self.frame_size * 10
         self.task.in_stream.input_buf_size = self.buffer_size
 
-    def set_stream_on(self) -> None:
+    def set_stream_enable(self) -> None:
         self.stream_trig = True
 
-    def set_stream_off(self) -> None:
+    def set_stream_disable(self) -> None:
         self.stream_trig = False
 
+    def set_write_file_enable(self) -> None:
+        self.write_file_trig = True
+
+    def set_write_file_disable(self) -> None:
+        self.write_file_trig = False
+
     def ready_read(self, callback_method) -> None:
-        self.callback_data_ptr.value.clear()
 
         self.task.register_every_n_samples_acquired_into_buffer_event(
             sample_interval=self.frame_size,
@@ -224,10 +205,10 @@ class NI9234(NIDAQ):
 
         self.chunk = np.zeros((self.task.number_of_channels, self.frame_size))
         self.stream_reader = NiStreamReaders.AnalogMultiChannelReader(self.task.in_stream)
-        self.callback_data_ptr.value.append(self.stream_reader)
-        self.callback_data_ptr.value.append(self.chunk)
 
     def start_task(self) -> None:
+        # self.stream_w0riter.set_file_name(
+        #    f'cli_{self.task.name}_{datetime.now().strftime("%Y%m%d_%H%M%S")}.csv')
         self.task.start()
 
     def start_streaming_period_time(self, time: Union[float, int]) -> None:
@@ -237,7 +218,7 @@ class NI9234(NIDAQ):
         number_of_samples = int(time * self.sample_rate)
         return self.task.in_stream.read(number_of_samples_per_channel=number_of_samples)
 
-    async def async_streaming(self) -> None:
+    async def async_start_task(self) -> None:
         self.task.start()
 
     def stop_task(self) -> None:
@@ -245,67 +226,79 @@ class NI9234(NIDAQ):
         print('Task is stopped!!')
 
     def close_task(self) -> None:
-        self.set_stream_off()
+        self.set_stream_disable()
         self.task.close()
+        if self.stream_writer.file != None:
+            self.stream_writer.close_file()
         print('Task is done!!')
+
+    def show_control_manual(self):
+        print('Press "s" to start, "q" to quit.')
+        print('While recording, press "p" to stop streaming, "q" to quit program.')
 
     async def key_switch_event(self) -> None:
         while True:
+
             await asyncio.sleep(0.1)
             if keyboard.is_pressed('s') and not self.stream_trig:
-                self.set_stream_on()
+                self.set_stream_enable()
                 self.start_task()
                 print('start streaming')
             elif keyboard.is_pressed('p'):
-                self.set_stream_off()
+                self.set_stream_disable()
                 print('stop streamming')
             elif keyboard.is_pressed('q'):
-                self.set_stream_off()
+                self.set_stream_disable()
                 self.close_task()
                 print('close task')
                 break
 
+    @staticmethod
+    def callback_method(task_handle, every_n_samples_event_type,
+                        number_of_samples, _):
+        global callback_obj_ptr
+        callback_obj_ptr.value.stream_reader.read_many_sample(callback_obj_ptr.value.chunk)
 
-def callback_NIDAQ(task_handle, every_n_samples_event_type,
-                   number_of_samples, callback_data):
-    stream_reader = niDAQ.callback_data_ptr.value[0]
-    chunk = niDAQ.callback_data_ptr.value[1]
-    stream_reader.read_many_sample(chunk)
+        if callback_obj_ptr.value.write_file_trig:
+            callback_obj_ptr.value.stream_writer.write(
+                chunk=callback_obj_ptr.value.chunk, transpose=True)
 
-    current_time = datetime.now().isoformat(sep=' ', timespec='milliseconds')
-    print(
-        f'Current time: {current_time} / Recording... every {number_of_samples} Samples callback invoked. stream trig: {niDAQ.stream_trig}\n'
-        'Press "p" to stop streaming, "q" to quit program.')
+        current_time = datetime.now().isoformat(sep=' ', timespec='milliseconds')
+        print(
+            f'Current time: {current_time} / Recording... every {number_of_samples} Samples callback invoked.')
 
-    if not niDAQ.stream_trig:
-        niDAQ.stop_task()
-    return 0
+        if not callback_obj_ptr.value.stream_trig:
+            callback_obj_ptr.value.stop_task()
+        return 0
 
+
+callback_obj_ptr: NIDAQ = None
 
 if __name__ == '__main__':
-    niDAQ = NI9234(device_name='NI_9234')
-    niDAQ.create_task(task_name='myTask')
-    niDAQ.add_accel_channel(0)
-    niDAQ.add_accel_channel(1)
-    # niDAQ.add_microphone_channel(2)
-    # niDAQ.add_microphone_channel(3)
-    niDAQ.set_sample_rate(25600)
-    niDAQ.set_frame_duration(100)
+    nidaq = NI9234(device_name='NI_9234')
+    callback_obj_ptr = nidaq.self_ptr
+    nidaq.create_task(task_name='myTask')
+    nidaq.add_accel_channel(0)
+    nidaq.add_accel_channel(1)
+    # nidaq.add_microphone_channel(2)
+    # nidaq.add_microphone_channel(3)
+    nidaq.set_sample_rate(25600)
+    nidaq.set_frame_duration(100)
 
-    niDAQ.ready_read(callback_method=callback_NIDAQ)
-
+    nidaq.ready_read(callback_method=nidaq.callback_method)
+    nidaq.show_control_manual()
     # method 1 async function with event loop
     # loop = asyncio.get_event_loop()
-    # tasks = [asyncio.ensure_future(niDAQ.key_switch_event())]
+    # tasks = [asyncio.ensure_future(nidaq.key_switch_event())]
     # loop.run_until_complete(asyncio.wait(tasks))
 
     # method 2 async run (similiar with method 1)
-    tasks = [niDAQ.key_switch_event()]
+    tasks = [nidaq.key_switch_event()]
     asyncio.run(asyncio.wait(tasks))
 
     # method 4 read with period time (not complete yet!!)
-    # niDAQ.start_streaming(callback_method=callback_NIDAQ)
-    # data = niDAQ.start_streaming_period_time(10)    # only read ch0??
+    # nidaq.start_streaming(callback_method=callback_nidaq)
+    # data = nidaq.start_streaming_period_time(10)    # only read ch0??
     # print(data.shape)
 
     print('Process done!')
