268975fd4981bdd1430eb19a2e7b23652adf47be
liupoyip <liupoyip@gmail.com>
2023-07-10 12:49:48 +0800

    LGTM!!

--- mysql_uploader/mysql_uploader.py
+++ mysql_uploader/mysql_uploader.py
@@ -2,7 +2,7 @@ import os
 import time
 import json
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler,FileSystemEvent
+from watchdog.events import FileSystemEventHandler, FileSystemEvent
 
 import mysql.connector
 import mysql.connector.cursor
@@ -10,10 +10,8 @@ from typing import Optional
 
 
 class MySQLDataUploader(FileSystemEventHandler):
-
-
     db = None
-    cursor:Optional[mysql.connector.cursor.MySQLCursor] = None
+    cursor: Optional[mysql.connector.cursor.MySQLCursor] = None
     host = None
     port = None
     user = None
@@ -25,15 +23,15 @@ class MySQLDataUploader(FileSystemEventHandler):
     values = None
     cfg_path = None
     cfg = None
-    current_data:tuple = None
+    current_data: tuple = None
     rawdata_table_name = None
+    task_cfg = None
 
-
-    def __init__(self,target_dir,cfg_path):
+    def __init__(self, target_dir, cfg_path, task_cfg):
         self.target_dir = target_dir
         self.cfg_path = cfg_path
+        self.task_cfg = task_cfg
         self.read_cfg()
-    
 
     def read_cfg(self):
         with open(self.cfg_path) as file:
@@ -49,12 +47,11 @@ class MySQLDataUploader(FileSystemEventHandler):
         self.data_types = cfg['data_types']
         self.values = cfg['values']
         self.rawdata_table_name = cfg['rawdata_table_name']
-        
+        self.task_name = cfg['task']
 
-    def set_current_data(self,current_data):
+    def set_current_data(self, current_data):
         # TODO: 之要串正確的data
         self.current_data = current_data
-
 
     def connect_to_server(self):
         print('connect to mysql server...', end='')
@@ -64,25 +61,22 @@ class MySQLDataUploader(FileSystemEventHandler):
             user=self.user,
             password=self.password)
         print('Done!')
-        
-    
+
     def use_database(self):
         sql = f'USE {self.db_name}'
         print(f'execute sql command: {sql}')
         self.cursor = self.db.cursor()
         self.cursor.execute(sql)
-    
 
     def create_database(self):
         sql = f'CREATE DATABASE IF NOT EXISTS {self.db_name}'
         print(f'execute sql command: {sql}')
         self.cursor = self.db.cursor()
         self.cursor.execute(sql)
-        
 
     def create_rawdata_table(self):
         sql = f'CREATE TABLE IF NOT EXISTS {self.rawdata_table_name} ('
-        for header,data_type in zip(self.headers,self.data_types):
+        for header, data_type in zip(self.headers, self.data_types):
             sql = f'{sql}{header} '
             sql = f'{sql}{data_type}, '
         sql = sql[:-2]
@@ -90,7 +84,6 @@ class MySQLDataUploader(FileSystemEventHandler):
 
         print(f'execute sql command: {sql}')
         self.cursor.execute(sql)
-    
 
     def append_rawdata(self):
         sql = f'INSERT INTO {self.rawdata_table_name} ('
@@ -103,47 +96,46 @@ class MySQLDataUploader(FileSystemEventHandler):
             sql = f'{sql}{value}, '
         sql = sql[:-2]
         sql = f'{sql})'
-        
+
         print(f'execute sql command: {sql}')
         print(f'insert value: {self.current_data}')
-        self.cursor.execute(sql,self.current_data)
+        self.cursor.execute(sql, self.current_data)
         self.db.commit()
 
-
-    def on_created(self, event:FileSystemEvent):
+    def on_created(self, event: FileSystemEvent):
         if event.is_directory:
-            
+
             # TODO: 找到新增的資料夾
             print(f'new directory created: {event.src_path}')
-            print(f'uploading file to database...',end='')
+            print(f'uploading file to database...', end='')
             self.create_database()
             self.use_database()
             self.create_rawdata_table()
 
             timestamp = os.path.basename(event.src_path)
-            dummy_data = ('task',timestamp,os.path.join(event.src_path,'cfg.json'))
+            dummy_data = (self.task_name, timestamp, os.path.join(
+                event.src_path, self.task_cfg))
             self.set_current_data(dummy_data)
             self.append_rawdata()
             print(f'Done!!')
-    
 
-# TODO: 之後要把task and export path從 ./models/record_cfg.json 撈出來
 
 uploader_cfg_path = 'cfg.json'
-with open(uploader_cfg_path,'r') as file:
+with open(uploader_cfg_path, 'r') as file:
     cfg = json.load(file)
-
 
 task_name = cfg['task']
 database_dir = cfg['database_dir']
-lab=cfg['lab']
+lab = cfg['lab']
 
 machine = cfg['machine']
 table_name = cfg['rawdata_table_name']
-target_dir = os.path.join(task_name,database_dir,lab,machine,table_name,task_name)
+target_dir = os.path.join(task_name, database_dir, lab,
+                          machine, table_name, task_name)
 print(f'target directory: {target_dir}')
 
-uploader = MySQLDataUploader(target_dir=target_dir,cfg_path=uploader_cfg_path)
+uploader = MySQLDataUploader(
+    target_dir=target_dir, cfg_path=uploader_cfg_path, task_cfg=cfg['task_cfg'])
 print('--- Start monitoring ---')
 print(f'Monitor folder: {target_dir}')
 
@@ -153,7 +145,7 @@ observer.schedule(uploader, target_dir, recursive=False)
 observer.start()
 try:
     while True:
-        time.sleep(1)
+        time.sleep(0.1)
 except KeyboardInterrupt:
     observer.stop()
 observer.join()
